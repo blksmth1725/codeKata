06020d7aa510e9c0c25e7bc9dc3fe73156a01685
Christian Sheen <christiansheen@MacBook-Pro.local>
2020-12-16 20:41:06 -0500

    NthPower function which takes in two arguments => an array, and a number

---
+++ CodeKata/8KYU/NthPower.js
@@ -0,0 +1,20 @@
+// This kata is from check py.checkio.org
+
+// You are given an array with positive numbers and a non-negative number N. You should find the N-th power of the element in the array with the index N. If N is outside of the array, then return -1. Don't forget that the first element has the index 0.
+
+// Let's look at a few examples:
+
+// array = [1, 2, 3, 4] and N = 2, then the result is 3^2 == 9;
+// array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
+
+// const nToNthPower = (array, n) => {
+//     const selectedElement = array[n]
+//     if(!selectedElement) {
+//         return -1;
+//     }
+//     return selectedElement ** n
+// }
+
+const nToNthPower = (array, n) => (array[n] ? array[n] ** n : -1);
+
+console.log(nToNthPower([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4));
